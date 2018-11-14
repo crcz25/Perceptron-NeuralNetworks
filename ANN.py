86466c1d5359b6d9d203eeb389196da6006846f5
Carlos Cueto <a01209474@itesm.mx>
2018-11-13 21:11:03 -0600

    report

--- ANN.py
+++ ANN.py
@@ -16,34 +16,34 @@ def calculate_output(weights, inputs):
         return 1
     return 0
 
-# Return None if it's not linearly 
+# Return None if it's not linearly
 def train_ann(training_data):
     number_inputs = len(training_data[0]) - 1
-    
+
     error_last_iteration = 100000000
 
     weights = [random() for x in range(number_inputs)]
-    
+
     iteration_number = 0
-    
+
     while(error_last_iteration > 0):
         error_iteration = 0
         for row in training_data:
             y_cap = calculate_output(weights, row[:-1])
             delta = row[-1] - y_cap
-            
+
             for i in range(len(weights)):
                 weights[i] += delta * learning_rate * row[i]
-            
+
             error_iteration += delta ** 2
-            
+
 
         iteration_number += 1
         if iteration_number > 5000:
             return None
-            
+
         error_last_iteration = error_iteration
-    
+
     return weights
 
 def process_input():
@@ -80,14 +80,14 @@ def main():
 
     print('\nTest data')
     pprint(test_data)"""
-    
+
     neuron = train_ann(training_data)
-    
+
     if neuron:
         for test in test_data:
-            
+
             print(calculate_output(neuron, tuple([1]) + test))
-            
+
     else:
         print('no solution found')
 
